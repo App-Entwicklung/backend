83f114c1ca6012e7b499becbe380ee47104a6320
Hase Florian <florian.hase@sap.com>
2022-09-07 21:35:21 +0200

    added basic server

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { opine } from './deps.ts';
+
+const app = opine();
+
+app.get('/', (_req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () =>
+  console.log('server has started on http://localhost:3000 🚀')
+);
---
+++ src/deps.ts
@@ -0,0 +1,1 @@
+export { opine } from 'https://deno.land/x/opine@2.3.3/mod.ts';
